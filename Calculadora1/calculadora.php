2e7ac4e4bf0d75e3b0d9f5d1ae68b57e825aa892
Masaano <mateusmartins27000@gmail.com>
2021-04-16 16:58:54 -0300

    revisado

--- Calculadora1/calculadora.php
+++ Calculadora1/calculadora.php
@@ -1,44 +1,64 @@
 <?php
-   $num1 = $_POST['num1'];
-   $num2 = $_POST['num2'];
-   $op= $_POST['operacao'];
+   if ($_SERVER["REQUEST_METHOD"]  == "POST") {
+      $num1 = $_POST['num1'];
+      $num2 = $_POST['num2'];
+      $op= $_POST['operacao'];
 
-   function soma(int $num1, int $num2){
-      $resul = $num1 + $num2;
-      return $resul;
+      function validarDados($num1, $num2, String $op){
+         $erroMsg = "";
+         if (!is_numeric($num1)) {
+            $erroMsg = "Valor 1 não é numerico";
+         }else if (!is_numeric($num2)) {
+               $erroMsg = "Valor 2 não é numerico";
+         }
+
+         if ($op == "divi" and $num2 == 0) {
+               $erroMsg = "Parametro b não pode ser 0 para divisão";
+         }
+         return $erroMsg;
    }
-
-   function subtracao(int $num1, int $num2){
-      $resul = $num1 - $num2;
-      return $resul;
-   }
-
-   function multiplicacao(int $num1, int $num2){
-      $resul = $num1 * $num2;
-      return $resul;
-   }
-
-   function divisao(int $num1, int $num2){
-      if($num2 == 0){
-         $resul = "invalido";
-      }else{
-         $resul = $num1 / $num2;
-      }
-      return $resul;
-   }
-
-   if($op == 'Soma'){
-      $c = soma($num1, $num2);
-   }else if($op == 'Subtracao'){
-      $c = subtracao($num1, $num2);
-   }else if($op == 'Multiplicacao'){
-      $c = multiplicacao($num1, $num2);
-   }else{
-      $c = divisao($num1, $num2);
       
+
+      function soma(float $num1, float $num2){
+         $resul = $num1 + $num2;
+         return $resul;
+      }
+
+      function subtracao(float $num1, float $num2){
+         $resul = $num1 - $num2;
+         return $resul;
+      }
+
+      function multiplicacao(float $num1, float $num2){
+         $resul = $num1 * $num2;
+         return $resul;
+      }
+
+      function divisao(float $num1, float $num2){
+         if($num2 == 0){
+            $resul = "invalido";
+         }else{
+            $resul = $num1 / $num2;
+         }
+         return $resul;
+      }
+      $vali = validarDados($num1,$num2,$op);
+      if($vali == ""){
+         if($op == 'soma'){
+            $c = soma($num1, $num2);
+         }else if($op == 'sub'){
+            $c = subtracao($num1, $num2);
+         }else if($op == 'mult'){
+            $c = multiplicacao($num1, $num2);
+         }else if($op == 'divi'){
+            $c = divisao($num1, $num2);      
+         } else{
+            $c = "Pare de mexer no codigo" ;
+         }
+      
+         echo "O resultado da operacao: $c";
+      }
+      echo $vali;
    }
-
-   echo "O resultado da operacao: $c";
    
-
 ?>       
